0a2d8deff8df56aa526499f12dfff29799a5aa64
hominhhoa39 <hominhhoa39@gmail.com>
2020-02-15 23:47:24 +0900

    delete vendor

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var cors = require('cors')
 
 var dictionary = require('./routes/dictionary');
 var conf = require('./util/config');
 
 var app = express();
+
+var whitelist = ['https://mh-dictionary.herokuapp.com/']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.get('/products', cors(corsOptions), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for a whitelisted domain.'});
+})
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
