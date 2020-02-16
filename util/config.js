cea79740482b202f2ab88c1aba81f4fa788eb852
hominhhoa39 <hominhhoa39@gmail.com>
2020-02-16 21:58:57 +0900

    change something

--- util/config.js
+++ util/config.js
@@ -1,5 +1,6 @@
 module.exports = Object.freeze({
     PORT: process.env.PORT || 3000,
-    CONNECT_STRING: process.env.MONGODB_URI || "mongodb://localhost:27017/minho_dict",
+    CONNECT_STRING: 'mongodb+srv://mhWordsAdmin:MHW0rdzAdmin20@cluster0-v3o5a.mongodb.net/test?retryWrites=true&w=majority',
+    //"mongodb://localhost:27017/minho_dict",
     WORD_DISPLAY_NUMBER: process.env.WORDS_DISP_NUMBER || 10
 });
